c9878c084413bb3b5cfa22a72ea1e2571ca8d682
megan <megan.schertz@gmail.com>
2021-04-23 17:50:55 -0500

    commit tree be7285affcf9b570a6f65ac2ed4723753088e504

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Write your code here!
+let element = document.createElement('div') 
+
+document.body.appendChild(element)
+
+let h1 = document.createElement('h1')
+element.appendChild(h1)
+
+h1.setAttribute('id', 'victory')
+
+let newHeader = document.querySelector('h1#victory');
+newHeader.innerHTML = "YOUR-NAME is the champion"
+
+element.appendChild(newHeader)
+
